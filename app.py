ff7c0981b76aa2df8308373ed02cec57b9fcad94
Paul Prince <paul.prince-ext@lizeo-group.com>
2020-08-18 18:14:07 +0200

    add a filter on budget to show 10 first results

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
 from flask import Flask, render_template, request
+import sqlite3
 import init_db
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Bienvenue'
-
-
-@app.route('/whoareyou')
 def index():
-    # return "<p>Tout fonctionne parfaitement</p>"
-
     return render_template('index.html')
 
-@app.route('/resultat',methods = ['POST'])
+
+@app.route('/resultat', methods=['POST'])
 def resultat():
-  result = request.form
-  n = result['nom']
-  p = result['prenom']
-  return render_template("resultat.html", nom=n, prenom=p)
+    result = request.form
+    prenom = result['prenom']
+    age = result['age']
+    gender = result['gender']
+    with_child = result['with_child']
+    place_of_residence = result['place_of_residence']
+    budget = result['budget']
+
+    where_query = ''
+    if budget:
+        where_query = ' Where price_eur < ' + budget
+
+    sql_query = 'SELECT * FROM Cars ' + where_query + ' Limit 10'
+
+    conn = get_db_connection()
+    posts = conn.execute(sql_query).fetchall()
+    conn.close()
+
+    return render_template("resultat.html", posts=posts, prenom=prenom)
+
 
 @app.route('/init')
 def init():
     return init_db.load_data()
+
+
+def get_db_connection():
+    conn = sqlite3.connect('CarStore3000.db')
+    conn.row_factory = sqlite3.Row
+    return conn
--- init_db.py
+++ init_db.py
@@ -8,10 +8,14 @@ def load_data():
     sqlite_connection = engine.connect()
 
     cars_df = pd.read_csv(cars_data_path)
-    cars_df = cars_df[cars_df.model.notnull()]
+    cars_df = cars_df[cars_df.maker.notnull()
+                      & cars_df.model.notnull()
+                      & cars_df.mileage.notnull()
+                      & cars_df.manufacture_year.notnull()
+                      & cars_df.price_eur.notnull()
+                      & cars_df.body_type.notnull()]
 
     sqlite_table = "Cars"
     cars_df.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
 
     return True
-
